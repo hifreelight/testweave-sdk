c6a328ee4e386a6ab78785ccb900593683983bc5
Frank <fengxuting@gmail.com>
2022-12-20 21:11:03 +0800

    did

---
+++ tests/did.test.ts
@@ -0,0 +1,63 @@
+import TestWeave from 'testweave-sdk';
+import Arweave from 'arweave';
+import { createContract, readContract, interactWrite, interactWriteDryRun } from 'smartweave';
+import fs from 'fs';
+import contractInitState from './fixtures/did.json';
+
+// npx ts-mocha ./tests/test.test.ts
+describe('testing SmartWeave interactions', function (): void {
+    this.timeout(200000);
+    it('should correctly create a new SmartWeave contract', async (): Promise<void> => {
+
+        const arweave = Arweave.init({
+            host: 'localhost',
+            port: 1984,
+            protocol: 'http',
+            timeout: 20000,
+            logging: true,
+        });
+
+        const testWeave = await TestWeave.init(arweave);
+
+        // import the sample contract init state
+        // load the contract as a string
+        const contractSource = fs.readFileSync('./tests/fixtures/did.js').toString();
+        // console.log(contractSource);
+        // create the contract and mine the transaction for creating it
+        const c = await createContract(arweave, testWeave.rootJWK, contractSource, JSON.stringify(contractInitState));
+        await testWeave.mine();
+
+        // read the contract before performing any interaction
+        const beforeTransaction = await readContract(arweave, c);
+        console.log(`Before interact write: ${JSON.stringify(beforeTransaction)}`)
+
+        // generate a wallet
+        const jkw = await arweave.wallets.generate();
+        const generatedAddr = await arweave.wallets.getAddress(jkw)
+
+        const rootAddr = await arweave.wallets.getAddress(testWeave.rootJWK);
+        console.log("rootAddr:" + rootAddr)
+
+        // interact with the contract
+        const iwt = await interactWrite(arweave, testWeave.rootJWK, c, {
+            function: 'register',
+            address: "address",
+            name: "hardy"
+        }, [], generatedAddr, '23999392')
+        console.log(`Interact write transaction: ${JSON.stringify(iwt)}`);
+
+        // mine the contract interaction transaction
+        await testWeave.mine();
+
+        // get the new balance of the generated address (it should be 23999392)
+        const generatedAddressBalance = await arweave.wallets.getBalance(generatedAddr)
+        console.log(generatedAddressBalance)
+
+        // read the contract after the interact write transaction (the generated wallet should own 5000 tokens)
+        const afterTransaction = await readContract(arweave, c);
+        console.log(`After interact write: ${JSON.stringify(afterTransaction)}`);
+        // return;
+    });
+
+});
+
---
+++ tests/fixtures/did.js
@@ -0,0 +1,85 @@
+// A simple name system:
+// Supports just one level of names.
+// Supports associating a address with a name.
+
+// Note: this is untested example atm.
+
+// TODO: require a minimum reward or burn of Ar to register a name.
+
+export function handle(state, action) {
+    if (action.input.function === 'register') {
+        ContractAssert(action.caller === state.owner, "Only the owner of this weeve can register");
+
+        if (typeof action.input.name !== 'string' || action.input.name.length < 3) {
+            throw new ContractError(`Invalid name provided: ${action.input.name}`)
+        }
+        if (typeof action.input.address !== 'string') {
+            throw new ContractError('Must provide address to be associated with the name')
+        }
+        if (state.names[action.input.name]) {
+            throw new ContractError('Name already registered')
+        }
+        state.names[action.input.name] = {
+            address: action.input.address
+        }
+        state.addresses[action.input.address] = {
+            name: action.input.name
+        }
+
+        return { state }
+    }
+
+    if (action.input.function === 'update') {
+        ContractAssert(action.caller === state.owner, "Only the owner of this weeve can register");
+
+        if (typeof action.input.name !== 'string' || action.input.name.length < 3) {
+            throw new ContractError(`Invalid name provided: ${action.input.name}`)
+        }
+        if (typeof action.input.address !== 'string') {
+            throw new ContractError('Must provide address to be associated with the name')
+        }
+        if (!state.names[action.input.name]) {
+            throw new ContractError('Name not registered')
+        }
+
+        state.names[action.input.name] = {
+            address: action.input.address
+        }
+        state.addresses[action.input.address] = {
+            name: action.input.name
+        }
+
+        return { state }
+    }
+
+    if (action.input.function === 'getName') {
+        if (typeof action.input.address !== 'string') {
+            throw new ContractError(`Invalid address provided: ${action.input.address}`)
+        }
+        if (!state.names[action.input.address]) {
+            throw new ContractError('Name not registered')
+        }
+
+        return state.names[action.input.address]
+    }
+
+    if (action.input.function === 'getAddress') {
+        if (typeof action.input.name !== 'string' || action.input.name.length < 3) {
+            throw new ContractError(`Invalid name provided: ${action.input.name}`)
+        }
+        if (!state.names[action.input.name]) {
+            throw new ContractError('Name not registered')
+        }
+
+        return state.names[action.input.name]
+    }
+
+    if (action.input.function === 'getList') {
+        const start = action.input.start
+        const limit = action.input.limit
+
+        return { result: state.addresses.slice(start, start + limit), total: state.addresses.length}
+    }
+
+    throw new ContractError('Invalid input')
+}
